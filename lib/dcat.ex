de0c69aa480dcd28897343277e17b8dc0fe6a775
Marcel Otto <marcelotto@gmx.de>
2024-08-07 14:40:50 +0200

    Add docs

--- lib/dcat.ex
+++ lib/dcat.ex
@@ -2,6 +2,11 @@ defmodule DCAT do
   @moduledoc """
   Implementation of the W3C DCAT 3 vocabulary.
 
+  The following features are provided:
+
+  - The `DCAT` module acts as the `RDF.Vocabulary.Namespace` for the DCAT vocabulary.
+  - Grax structures for all DCAT classes and their properties.
+
   See https://www.w3.org/TR/vocab-dcat-3/.
   """
 
